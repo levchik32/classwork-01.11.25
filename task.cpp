2e053e279cabcb1b5d975a45c2092f9bed8ea471
Carrot_lord <lev.saldaev@gmai.com>
2025-11-01 17:25:00 +0300

    function output implemented + some fixes

--- task.cpp
+++ task.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
 int** convert(const int* t, size_t n, const size_t* lns, size_t rows);
-int** make(int r, int c);
+int** make(int r, const size_t* lns);
 void rm(int** mtx, int r);
+void output(int** mtx, int r, const size_t* lns);
 
 int main()
 {
@@ -23,6 +24,8 @@ int** convert(const int* t, size_t n, const size_t* lns, size_t rows)
       k += 1;
     }
   }
+
+  return mtx;
 }
 
 int** make(size_t r, const size_t* lns)
@@ -50,4 +53,17 @@ void rm (int** mtx, int r)
     delete[] mtx[i];
   }
   delete[] mtx;
+}
+
+void output(int** mtx, int r, const size_t* lns)
+{
+  for (size_t i = 0; i < r; ++i)
+  {
+    size_t c = lns[i];
+    for (size_t j = 0; j < c - 1; ++j)
+    {
+      std::cout << mtx[i][j] << " ";
+    }
+    std::cout << mtx[i][c - 1] << "\n";
+  }
 }
